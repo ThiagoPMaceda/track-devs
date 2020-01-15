feaf7b696c8175350d3cef06572cdbc9560a447e
Thiago P <thiagopmaceda@gmail.com>
2020-01-14 22:10:18 -0300

    adicionado mongoose e conexao com banco de dados mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
 
+mongoose.connect('mongodb+srv://tp_nosqlatlas:RI3OIMh7hAtZSaBq@cluster0-2hk2x.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+app.use(express.json);
+
 app.get('/', (req, res) => {
-    return res.json({ message: 'Server funcionando' });
+    return res.json({ message: 'Server teste' });
 });
 
 app.listen(3333);
