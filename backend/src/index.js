90d0460522677834175ae9c34a9968d3295fb9e7
Thiago P <thiagopmaceda@gmail.com>
2020-01-18 17:50:50 -0300

    Alterações para guardar as informacoes de geolocalizacao do Dev, separando a regra de negocio em um Controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,8 @@ const app = express();
 mongoose.connect('mongodb+srv://tp_nosqlatlas:RI3OIMh7hAtZSaBq@cluster0-2hk2x.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
+    useCreateIndex: true,
+});
 
 app.use(express.json());
 app.use(routes);
