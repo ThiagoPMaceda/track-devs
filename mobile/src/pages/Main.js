9a3df480410f7e36f1959d33177377a6801eabca
Thiago P <thiagopmaceda@gmail.com>
2020-01-19 20:59:04 -0300

    criada busca por tecs

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, View, Text } from "react-native";
+import { StyleSheet, Image, View, Text, TouchableOpacity } from "react-native";
 import MapView, { Marker, Callout } from "react-native-maps";
 import { requestPermissionsAsync, getCurrentPositionAsync } from "expo-location";
+import { TextInput } from 'react-native-gesture-handler';
+import { MaterialIcons } from "@expo/vector-icons";
 
 function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
@@ -33,22 +35,37 @@ function Main({ navigation }) {
     }
 
     return (
-        <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={{ latitude: -23.525221, longitude: -46.4512797}}>
-                <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/30845442?s=460&v=4' }} />
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={{ latitude: -23.525221, longitude: -46.4512797}}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/30845442?s=460&v=4' }} />
 
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', { github_username: 'ThiagoPMaceda' });
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Thiago Maceda</Text>
-                        <Text style={styles.devBio}>Formado em Ciencia da Computação pela Uninove, realizando projetos em desenvolvimento web para aprimorar meus conhecimentos.</Text>
-                        <Text style={styles.devTechs}>ReactJS, Node.js, React Native</Text>
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'ThiagoPMaceda' });
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Thiago Maceda</Text>
+                            <Text style={styles.devBio}>Formado em Ciencia da Computação pela Uninove, realizando projetos em desenvolvimento web para aprimorar meus conhecimentos.</Text>
+                            <Text style={styles.devTechs}>ReactJS, Node.js, React Native</Text>
 
-                    </View>
-                </Callout>
-            </Marker>   
-        </MapView>
+                        </View>
+                    </Callout>
+                </Marker>   
+            </MapView>
+            <View style={styles.searchForm}>
+                    <TextInput
+                        style={styles.searchInput}
+                        placeholder="Buscar devs por techs..."
+                        placeholderTextColor="#999"
+                        autoCapitalize="words"
+                        autoCorrect= {false}
+                    />   
+
+                    <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                        <MaterialIcons name="my-location" size={20} color={"#FFF"} />
+                    </TouchableOpacity> 
+            </View>
+        </>    
     ); 
 }
 
@@ -81,7 +98,43 @@ const styles = StyleSheet.create({
 
     devTechs: {
         marginTop: 5,
-    }
+    },
+
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+         elevation: 2,
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4DFF',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
+    },
 });
 
 export default Main;
